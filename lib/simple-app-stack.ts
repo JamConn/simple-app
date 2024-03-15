24d2dbe37ff205e2babe92a2533214fa7bb57dc7
JamConn <20085217>
2024-03-15 22:04:20 +0000

    Lambda function to get all movies.

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -72,8 +72,22 @@ export class SimpleAppStack extends cdk.Stack {
         allowedOrigins: ["*"],
       },
     });
-
-    moviesTable.grantReadData(getMovieByIdFn)
+    
+    const getAllMoviesFn = new lambdanode.NodejsFunction(
+      this,
+      "GetAllMoviesFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambdas/getAllMovies.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+      );
 
     new cdk.CfnOutput(this, "Get Movie Function Url", { value: getMovieByIdURL.url });
 
